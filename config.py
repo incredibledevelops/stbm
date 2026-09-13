1cd8146848129134898a7df3c449dffe46aabf8d
Incredible Develops <incredibledevelops@gmail.com>
2026-09-13 04:22:03 -1200

    SQLite to MySQL Fix

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///stbm.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root:@localhost/stbm_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # File Upload Configuration
